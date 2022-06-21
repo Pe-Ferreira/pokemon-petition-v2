fb2c292e9cb384b0f1ca115fa5e6764721300f03
Pedro Ferreira <pe.ferreira@outlook.com>
2022-06-21 20:39:51 -0300

    getting countries from restcountries.com

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+const countriesAPIUrl = 'https://restcountries.com/v3.1/all';
+
+fetch(countriesAPIUrl)
+.then(data => data.json())
+.then(res => {
+    const select = document.getElementById('country');
+    let countries = [];
+
+    for(index in res) {
+        countries.push(res[index].name.common);
+    }
+    
+    const sortedCountries = countries.sort();
+
+    for(index in sortedCountries) {
+        select.options[select.options.length] = new Option(sortedCountries[index], sortedCountries[index]);
+    }
+});
+
 function vote() {
-    alert('Thanks for voting!');
+    const name = document.getElementById('name').value;
+    console.log(name);
+
+    const country = document.getElementById('country').value;
+    console.log(country);
 }

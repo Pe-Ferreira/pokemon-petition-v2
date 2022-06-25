c8e850bdcde8903e5961b1e3d0817f20639fde4a
Pedro Ferreira <pe.ferreira@outlook.com>
2022-06-25 14:06:58 -0300

    counting animation

--- index.js
+++ index.js
@@ -18,23 +18,21 @@ const analytics = getAnalytics(app);
 const countriesAPIUrl = 'https://restcountries.com/v3.1/all';
 
 fetch(countriesAPIUrl)
-.then(data => data.json())
-.then(res => {
-    const select = document.getElementById('country');
-    let countries = [];
+    .then(data => data.json())
+    .then(res => {
+        const select = document.getElementById('country');
+        let countries = [];
 
-    for(let index in res) {
-        countries.push(res[index].name.common);
-    }
-    
-    const sortedCountries = countries.sort();
+        for (let index in res) {
+            countries.push(res[index].name.common);
+        }
 
-    for(let index in sortedCountries) {
-        select.options[select.options.length] = new Option(sortedCountries[index], sortedCountries[index]);
-    }
-});
+        const sortedCountries = countries.sort();
 
-loadVoteCounter();
+        for (let index in sortedCountries) {
+            select.options[select.options.length] = new Option(sortedCountries[index], sortedCountries[index]);
+        }
+    });
 
 function vote() {
     const name = document.getElementById('name').value;
@@ -66,21 +64,36 @@ function voteFeedback(name) {
     document.location.reload();
 }
 
+function animateValue(obj, start, end, duration) {
+    let startTimestamp = null;
+    const step = (timestamp) => {
+        if (!startTimestamp) startTimestamp = timestamp;
+        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+        obj.innerHTML = Math.floor(progress * (end - start) + start);
+        if (progress < 1) {
+            window.requestAnimationFrame(step);
+        }
+    };
+    window.requestAnimationFrame(step);
+}
+
 function loadVoteCounter() {
     const dbRef = ref(getDatabase());
     get(child(dbRef, `votes/`)).then((snapshot) => {
-      if (snapshot.exists()) {
-        const dataKeys = Object.keys(snapshot.val());
+        if (snapshot.exists()) {
+            const dataKeys = Object.keys(snapshot.val());
 
-        const voteCounter = document.getElementById('vote-counter');
-        voteCounter.innerHTML = dataKeys.length.toString();
-        
-      } else {
-        console.log('No data available');
-      }
+            const voteCounter = document.getElementById('vote-counter');
+            animateValue(voteCounter, 0, dataKeys.length, 1000);
+            // voteCounter.innerHTML = dataKeys.length.toString();
+
+        } else {
+            console.log('No data available');
+        }
     }).catch((error) => {
         console.error(error);
     });
 }
 
+loadVoteCounter();
 window.vote = vote;
